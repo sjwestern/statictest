7773abca623d8f5811ff8f9a26224ceff235a974
Scott Western <s.j.western@gmail.com>
2024-12-26 15:23:19 +1030

    Add functions

---
+++ api/TestFunction1.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace StaticTest.Functions
+{
+    public class TestFunction1
+    {
+        private readonly ILogger<TestFunction1> _logger;
+
+        public TestFunction1(ILogger<TestFunction1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("TestFunction1")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
