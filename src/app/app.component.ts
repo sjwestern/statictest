996d3812d9b39a97fe84c992f9b58c7f7b6fd444
Scott Western <s.j.western@gmail.com>
2025-01-25 14:10:23 +1030

    Upgrade to dotnet 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   value = 'World';
+  constructor(private http: HttpClient) {
+    this.http.get('/api/TestFunction1').subscribe((data: any) => {
+      this.value = data.title;
+    });
+  }
 }
